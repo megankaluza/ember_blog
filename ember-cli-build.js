e3e43f5bfdc750cdd6b94d837513da3e54322999
Laura Krouse and Megan Kaluza <laura+megan@>
2016-10-26 14:53:03 -0700

    show updates to color palette and font cahnges to headings

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,9 +20,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  // app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
-  // return
-  // app.toTree();
-  // app.import ("<link href='https:fonts.googleapis.com/css?family=Sigmar+One' rel='stylesheet'>");
+  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+  app.import('https://fonts.googleapis.com/css?family=Sigmar+One');
   return app.toTree();
 };
