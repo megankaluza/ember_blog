e6303d27e9a96bfd22b2a9676c97f2694e7d60ce
Laura Krouse and Megan Kaluza <laura+megan@>
2016-10-27 12:57:42 -0700

    show update and delete button on the route page

--- app/routes/entry.js
+++ app/routes/entry.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('entry', params.entry_id);
+  },
+  updateEntryForm: false,
+  actions: {
+    update(entry, params) {
+      this.sendAction('update', entry, params);
+    },
+    delete(entry) {
+      if (confirm('Are you sure you want to delete?')) {
+        this.sendAction('destroyEntry', entry);
+      }
+    }
   }
 });
